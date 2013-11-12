5fd04cf8ec52abcc4f5b1783285b19c540a5312e
autokad <karl_dailey@hotmail.com>
2013-11-12 09:32:03 -0500

    blah 2

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,7 +8,7 @@ class Pin < ActiveRecord::Base
 
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>" }
-  scope:
+
   def image_remote_url=(url_value)
   	self.image = URI.parse(url_value) unless url_value.blank?
   	super
